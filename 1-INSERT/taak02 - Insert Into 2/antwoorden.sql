a8d78a59b46cb9e023b5a6fb1d670e10f5a121de
BruceAmende <bruceamende32@gmail.com>
2020-10-22 11:36:18 +0200

    taak 1/02 af

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,6 @@
 
+INSERT INTO `studenten`( `voornaam`, `achternaam`, `geboortedatum`, `woonplaats`) VALUES ('Joris','Pieterson','2002-07-12','Amstelveen')
+
+INSERT INTO `studenten`( `voornaam`, `achternaam`, `geboortedatum`, `woonplaats`) VALUES ('Nelis','Jacobs','2002-03-14','Amstelveen')
+
+INSERT INTO `studenten`( `voornaam`, `achternaam`, `geboortedatum`, `woonplaats`) VALUES ('Drake','Draco','2002-03-14','LA')
